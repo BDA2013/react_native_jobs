7ce25976db2cf745315030c613457f52b44078f6
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-28 12:55:48 -0400

    Bookmarked to continue later for job details

---
+++ app/job-details/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobDetails = () => {
+  return (
+    <div>JobDetails</div>
+  )
+}
+
+export default JobDetails
+
+// Continue from this link: https://youtu.be/mJ3bGvy0WAY?si=ZSUxmqCvM8PhbgWn&t=4968
